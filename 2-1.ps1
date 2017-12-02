1c0294469647139ca285ec1a66e611126257e532
isitLoVe <isitl0v3@gmail.com>
2017-12-02 20:15:24 +0100

    2

---
+++ 2-1.ps1
@@ -0,0 +1,39 @@
+﻿$val = get-content -path "D:\git\adventofcode\in\day2.txt"
+
+
+$sum = 0
+$checksum = 0
+
+
+foreach ($row in $val) {
+    write-host "row: $row"
+    [int]$highnum = 0 #hax
+    [int]$lownum = 999999 #dirtyhax
+
+    $rownum = $row.Split("`t")
+
+    foreach ($strnum in $rownum) {
+    
+    $num = [int]$strnum #F**K STRINGS disguised as INTEGERS
+
+    write-host "current num $num"
+    
+        if ($num -ge $highnum) {
+            write-host "yey $num is greater than $highnum"
+            $highnum = $num
+        }
+        
+
+        if ($num -le $lownum) {
+                write-host "yey $num is lower than $lownum"
+            $lownum = $num
+        }
+
+    }
+    $sum = $highnum - $lownum 
+    $checksum = $checksum + $sum
+    write-host "current `t $highnum-$lownum=$sum"
+
+}
+
+write-host "checksum: $checksum"
---
+++ 2-2.ps1
@@ -0,0 +1,27 @@
+﻿$val = get-content -path "D:\git\adventofcode\in\day2.txt"
+#$val = get-content -path "D:\git\adventofcode\in\day2test.txt"
+
+$sum = 0
+$checksum = 0
+$div = $null
+
+
+foreach ($row in $val) {
+    write-host "row: $row"
+
+    $rowarr = $row.Split("`t")
+
+    foreach ($strnum in $rowarr) {
+        $num = [int]$strnum #F**K STRINGS disguised as INTEGERS
+ 
+        $div = $rowarr | Where-Object {$_ -ne $num} | Where-Object {$_ % $num -eq 0}
+        
+        if ($div){
+            write-host "division $div / $num = $($div/$num)"
+            #$checksum = $checksum + $div / $num
+            $checksum += $div / $num
+        }
+    }
+}
+
+write-host "checksum: $checksum"
