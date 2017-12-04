fcac3ed75f5e7ab35e313d98a6c40c5fa1a4a57f
Luise <isitl0v3@gmail.com>
2017-12-04 09:32:21 +0100

    4-1

---
+++ 4-1.ps1
@@ -0,0 +1,14 @@
+﻿$val = get-content -path "D:\git\adventofcode\in\day4.txt"
+#$val = get-content -path "D:\git\adventofcode\in\day4test.txt"
+
+$dupes = 0
+
+foreach ($passphrase in $val) {
+    $words = $passphrase.Split(" ")
+    if ($words | group-object | Where-Object -FilterScript {$_.Count -gt 1 }) {
+        write-host "$words dupes found"
+        $dupes++
+    }
+}
+
+Write-host "Answer: $($($val.Count) - $dupes)"
