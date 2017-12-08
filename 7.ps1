58ac636ff01e24aae39081c47ceee9ea32e66261
Luise <isitl0v3@gmail.com>
2017-12-08 13:27:21 +0100

    7 incomplete

---
+++ 7.ps1
@@ -0,0 +1,12 @@
+﻿$in = Get-Content -Path "C:\Users\adm.spatt\Desktop\day7.txt"
+$in = Get-Content -Path "C:\Users\adm.spatt\Desktop\day7test.txt"
+
+[regex]$regexsingle = "(?mi)^(?<name0>\w+) \((?<weight>\d+)\)$"
+
+[regex]$regexmulti1 = "(?mi)^(?<name0>\w+) \((?<weight>\d+)\) -> (?<name1>\w+)$"
+
+
+foreach ($line in $in) {
+$line
+}
+
